8d59961ad43754f0be52dbcc64e4f761efdd8c45
Faizan Khalid <IamFaizanKhalid@gmail.com>
2021-01-10 22:26:26 +0500

    Integrated Rocket

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+#![feature(proc_macro_hygiene, decl_macro)]
+
+#[macro_use] extern crate rocket;
+
+#[get("/")]
+fn index() -> &'static str {
+    "Hello, world!"
+}
+
+#[get("/ping")]
+fn ping() -> &'static str {
+    "Pong"
+}
+
 fn main() {
-    println!("Hello world!");
+    rocket::ignite()
+        .mount("/", routes![index,ping])
+        .launch();
 }
